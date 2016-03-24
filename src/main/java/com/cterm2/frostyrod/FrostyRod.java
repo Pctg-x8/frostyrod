069dea99b368b104ac31a05e6c9e2fd2d1ccbb4a
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-03-25 02:35:11 +0900

    add base of item and language file

--- src/main/java/com/cterm2/frostyrod/FrostyRod.java
+++ src/main/java/com/cterm2/frostyrod/FrostyRod.java
@@ -1,23 +1,33 @@
 package com.cterm2.frostyrod;
 
+import com.cterm2.frostyrod.item.ItemFrostyRod;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 // Mod Entry Class
 @Mod(modid = FrostyRod.ID, name = FrostyRod.Name, version = FrostyRod.Version)
 public class FrostyRod
 {
-	public static final FrostyRod instance = new FrostyRod();
-
 	static final String ID = "frostyrod";
 	static final String Name = "Frosty Rod";
 	static final String Version = "1.0.0";
+
+	private static final CreativeTabs ctab = new CreativeTabs(FrostyRod.ID)
+	{
+		@Override
+		public Item getTabIconItem() { return Items.stick; }
+	};
+	private static final ItemFrostyRod item = new ItemFrostyRod();
 
 	// Initialize the mod
 	@EventHandler
 	public void init(FMLInitializationEvent e)
 	{
-
+		GameRegistry.registerItem(item.setCreativeTab(ctab), "itemFrostyRod");
 	}
 }
---
+++ src/main/java/com/cterm2/frostyrod/item/ItemFrostyRod.java
@@ -0,0 +1,12 @@
+package com.cterm2.frostyrod.item;
+
+import net.minecraft.item.Item;
+
+public class ItemFrostyRod extends Item
+{
+	public ItemFrostyRod()
+	{
+		this.setUnlocalizedName("frostyRod");
+		this.setMaxStackSize(1);
+	}
+}
