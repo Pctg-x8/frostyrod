8ead8684e99bd5942f356d159ee2941f1057029f
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-03-25 04:01:46 +0900

    add effect

--- src/main/java/com/cterm2/frostyrod/item/ItemFrostyRod.java
+++ src/main/java/com/cterm2/frostyrod/item/ItemFrostyRod.java
@@ -3,12 +3,16 @@ package com.cterm2.frostyrod.item;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.S2APacketParticles;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 
 // Main item class of Frosty Rod
@@ -16,6 +20,7 @@ public class ItemFrostyRod extends Item
 {
 	public ItemFrostyRod()
 	{
+		// Cannot stack
 		this.setUnlocalizedName("frostyRod");
 		this.setMaxStackSize(1);
 	}
@@ -36,17 +41,20 @@ public class ItemFrostyRod extends Item
 				if(block == Blocks.dirt || block == Blocks.grass)
 				{
 					// To dirt or grass: freeze target block
-					this.freeze(world, pos);
-					return true;
+					this.freeze((EntityPlayerMP)player, world, pos);
 				}
 			}
-			return false;
+			return true;
 		}
 	}
 
 	// Freeze block
-	private void freeze(World world, BlockPos pos)
+	private void freeze(EntityPlayerMP player, World world, BlockPos pos)
 	{
-		world.setBlockState(pos, Blocks.ice.getDefaultState());
+		// Because executed in server, tell to client to spawn particles
+		player.playerNetServerHandler.sendPacket(new S2APacketParticles(EnumParticleTypes.EXPLOSION_NORMAL, true,
+				pos.getX() + 0.5f, pos.getY() + 0.5f, pos.getZ() + 0.5f, 0.0f, 0.0f, 0.0f, 0.1f, 20, new int[0]));
+		// Place ice block with notifying
+		world.setBlockState(pos, Blocks.ice.getDefaultState(), 3);
 	}
 }
