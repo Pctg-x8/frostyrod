0129c56c2dd5877fdf74acc9bc8649c74b49b135
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-03-25 02:55:26 +0900

    add replacement if dirt or grass

--- src/main/java/com/cterm2/frostyrod/item/ItemFrostyRod.java
+++ src/main/java/com/cterm2/frostyrod/item/ItemFrostyRod.java
@@ -1,8 +1,10 @@
 package com.cterm2.frostyrod.item;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
@@ -30,9 +32,21 @@ public class ItemFrostyRod extends Item
 			IBlockState blockState = world.getBlockState(pos);
 			if(blockState != null)
 			{
-				System.out.println("Item used to " + blockState.getBlock());
+				Block block = blockState.getBlock();
+				if(block == Blocks.dirt || block == Blocks.grass)
+				{
+					// To dirt or grass: freeze target block
+					this.freeze(world, pos);
+					return true;
+				}
 			}
-			return true;
+			return false;
 		}
+	}
+
+	// Freeze block
+	private void freeze(World world, BlockPos pos)
+	{
+		world.setBlockState(pos, Blocks.ice.getDefaultState());
 	}
 }
